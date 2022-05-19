2442534d8296b1ffeb09acae3aecf79498b03c01
HobbesWSL <jessewilson@outlook.com>
2022-05-19 19:41:13 +1200

    remove all api logic - just serve react app=

--- server/server.js
+++ server/server.js
@@ -1,17 +1,14 @@
 // Imports
 const path = require("path");
 const express = require("express");
-const apiRoutes = require("./routes/api");
 
 // Init
 const server = express();
 
 // Middleware / Setup
-server.use(express.json());
 server.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-server.use("/api/cohorts/", apiRoutes);
 server.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
