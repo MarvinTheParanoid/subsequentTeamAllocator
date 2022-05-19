f0b3b399863a6fa9cf764842ba3904fbb2eec91c
HobbesWSL <jessewilson@outlook.com>
2022-05-19 20:01:58 +1200

    remove router and redux

--- client/index.js
+++ client/index.js
@@ -1,17 +1,8 @@
+// Imports
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-
-// import store from './store.js'
 import App from "./components/App";
 
 // Inject into page
 const root = createRoot(document.getElementById("app"));
-root.render(
-  // {/* <Provider store={store}> */}
-  <Router>
-    <App />
-  </Router>
-  // </Provider>
-);
+root.render(<App />);
