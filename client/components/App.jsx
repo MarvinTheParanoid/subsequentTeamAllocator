bc2553c8af3281824e4b0ce53bd637748f6d305d
HobbesWSL <jessewilson@outlook.com>
2022-05-19 20:14:13 +1200

    add css reset

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,13 @@
 // Imports
 import React from "react";
+import { CssBaseline } from "@mui/material";
 
 //Component imports
-
 
 function App() {
   return (
     <div>
+      <CssBaseline />
       <h1>So fresh and so clean</h1>
     </div>
   );
