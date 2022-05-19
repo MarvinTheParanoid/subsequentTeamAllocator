1fd85cebfe495ea3e0fb55f47db5ee4ad804cc57
HobbesWSL <jessewilson@outlook.com>
2022-05-20 08:42:18 +1200

    make header static

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { AppBar, Toolbar, Typography, Switch, SvgIcon  } from '@mui/material'
-import { SiMicrogenetics } from 'react-icons/si'
+import React from "react";
+import { AppBar, Toolbar, Typography, Switch, SvgIcon } from "@mui/material";
+import { SiMicrogenetics } from "react-icons/si";
 
 // eslint-disable-next-line react/prop-types
-function Header({mode, changeMode}) {
+function Header({ mode, changeMode }) {
   return (
-    <AppBar position="fixed" color="primary">
+    <AppBar position="static" color="primary">
       <Toolbar>
-        <SvgIcon component={SiMicrogenetics} fontSize='large' inheritViewBox sx={{mr : 2}}/>
-        <Typography variant="h3" component='h1' sx={{ flexGrow: 1 }}>
+        <SvgIcon component={SiMicrogenetics} fontSize="large" inheritViewBox sx={{ mr: 2 }} />
+        <Typography variant="h3" component="h1" sx={{ flexGrow: 1 }}>
           Allocar
         </Typography>
         <Switch checked={mode} onClick={changeMode} />
       </Toolbar>
     </AppBar>
-  )
+  );
 }
 
-export default Header
+export default Header;
