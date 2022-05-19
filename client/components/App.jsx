79fd3d16f89e87c7d91d9a9c0ea385ee1b2258fe
HobbesWSL <jessewilson@outlook.com>
2022-05-19 20:51:13 +1200

    add basic dark theme

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,28 @@
 // Imports
-import React from "react";
-import { CssBaseline } from "@mui/material";
+import React, { useState, useMemo } from "react";
+import { CssBaseline, ThemeProvider, Paper, Typography, Switch } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { getTheme } from "../theme";
 
 //Component imports
 
+// App
 function App() {
+  // Theme
+  // create function in theme.js that first checks localStorage, then prefersDarkMode, otherwise false
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [ darkMode, setDarkMode ] = useState(() => prefersDarkMode);
+  const changeMode = () => setDarkMode(!darkMode) //would need to update local storage
+  const theme = useMemo(() => getTheme(darkMode), [darkMode]);
+
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <h1>So fresh and so clean</h1>
-    </div>
+      <Paper style={{ height: "100vh" }}>
+        <Typography variant="h1">Hello, World!</Typography>
+        <Switch checked={darkMode} onClick={changeMode} />
+      </Paper>
+    </ThemeProvider>
   );
 }
 
